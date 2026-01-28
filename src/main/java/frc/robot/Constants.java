7056871c705ac01cb38953fabc7269ff375d3116
Samhith Dewal <dewalsamhith@gmail.com>
2026-01-28 18:36:14 -0500

    Added Constants2026 & ConstantsTestbed2026 files.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,17 +12,8 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants extends GameConstants{
-    
-    public static final int ROLLER_MOTOR_ID = 1;
-    public static final int TILT_MOTOR_ID = 2;
-    public static final int INTAKE_MOTOR_ID = 3;
-    
-    public static final double DRIVE_BASE_WIDTH = 0.635;
-    public static final double DRIVE_BASE_LENGTH = 0.635;
-    public static final double INITIAL_ROBOT_HEIGHT = 0;
 
-    public static final int INTAKE_DIGITAL_INPUT_CHANNEL = 0;
+public class Constants extends Constants2026 {
 
 }
 
---
+++ src/main/java/frc/robot/ConstantsTestbed2026.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+public class ConstantsTestbed2026 extends Constants2026 {
+    
+    /**
+   * To use the TestBed constants, change Constants.java to read: public class Constants extends
+   * ConstantsTestbedClean2025
+   *
+   * <p>instead of: public class Constants extends Constants2025
+   *
+   * <p>Then revert changes to Constants.java and ConstantsTestBed.java before committing.
+   */
+
+     /**
+   * This an example of overriding a specific motor ID for testing, no changes to this file should
+   * be committed to Git.
+   
+   * public static final int EXAMPLE_ID = 99;
+   */
+
+}

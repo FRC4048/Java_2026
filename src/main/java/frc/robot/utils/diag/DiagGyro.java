783767d3debe0c2050df8d771ff46ca7c9243d1f
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2026-01-29 19:41:42 -0500

    initial commit (from last year no changes) (broken)

---
+++ src/main/java/frc/robot/utils/diag/DiagGyro.java
@@ -0,0 +1,18 @@
+package frc.robot.utils.diag;
+
+
+import frc.robot.utils.logging.io.gyro.ThreadedGyro;
+
+public class DiagGyro extends DiagDistanceTraveled {
+    private final ThreadedGyro gyro;
+
+    public DiagGyro(String title, String name, double requiredTravel, ThreadedGyro gyro) {
+        super(title, name, requiredTravel);
+        this.gyro = gyro;
+    }
+
+    @Override
+    protected double getCurrentValue() {
+        return gyro.getGyroValue();
+    }
+}

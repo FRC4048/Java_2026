4c9e03f4319e027637a762fcbd7e7e72f929eae1
Sahiltheram <sahil.a.thadani@gmail.com>
2026-01-30 20:44:13 -0500

    path tests

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -9,9 +9,9 @@ import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 
 public class Drive extends SubsystemBase{
-    private final PIDController xController = new PIDController(.1, 0.0, 0.0);
-    private final PIDController yController = new PIDController(.1, 0.0, 0.0);
-    private final PIDController headingController = new PIDController(.1, 0.0, 0.0);
+    private final PIDController xController = new PIDController(.01, 0.0, 0.0);
+    private final PIDController yController = new PIDController(.01, 0.0, 0.0);
+    private final PIDController headingController = new PIDController(.01, 0.0, 0.0);
     private SwerveSubsystem subsystem;
     
     public Drive(SwerveSubsystem subsystem) {

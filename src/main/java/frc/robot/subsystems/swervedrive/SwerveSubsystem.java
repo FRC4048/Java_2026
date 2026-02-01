8d1fb47d895e6b6665c2a58b97ea9d583bf5ff2b
Jackson Wess <wessjacson93@gmail.com>
2026-02-01 16:24:01 -0500

    Commented out code

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -106,7 +106,8 @@ public class SwerveSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        addVisionMeasurement(new Pose2d(new Translation2d(16, 2), new Rotation2d()));
+        //add vision pose here
+        //addVisionMeasurement(new Pose2d(new Translation2d(16, 2), new Rotation2d()));
     }
 
     @Override

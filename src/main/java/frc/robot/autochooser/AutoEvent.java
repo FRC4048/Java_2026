ae8f723ee11bd3ddd647119f0faa681d3489eb13
Raayed Mohammed <raayed.mo@gmail.com>
2026-02-06 18:27:35 -0500

    Got rid of unnecessary things.

--- src/main/java/frc/robot/autochooser/AutoEvent.java
+++ src/main/java/frc/robot/autochooser/AutoEvent.java
@@ -1,9 +1,5 @@
 package frc.robot.autochooser;
 
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Wrapper Class, that Contains a {@link frc.robot.autochooser.AutoAction} and a {@link
  * frc.robot.autochooser.FieldLocation}
@@ -25,34 +21,4 @@ public class AutoEvent {
     return location;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    AutoEvent autoEvent = (AutoEvent) o;
-    return action.equals(autoEvent.action) && location.equals(autoEvent.location);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(action, location);
-  }
-
-  @Override
-  public String toString() {
-    return "AutoEvent[action = " + action.toString() + ", location = " + location.toString() + "]";
-  }
-
-  public static AutoEvent fromString(String name) {
-    // Regex to capture the values inside 'action = ' and 'location = '
-    Pattern pattern = Pattern.compile("AutoEvent\\[action = (.*), location = (.*)\\]");
-    Matcher matcher = pattern.matcher(name);
-
-    if (matcher.find()) {
-        AutoAction action = AutoAction.fromName(matcher.group(1));
-        FieldLocation location = FieldLocation.fromName(matcher.group(2));
-        return new AutoEvent(action, location);
-    }
-    throw new IllegalArgumentException("Invalid format");
-  }
 }

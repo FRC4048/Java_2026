7a302829b406c7e4ddf4a1be50bbb2494a185843
Joseph Kantrowitz <josephkantrowitz18@gmail.com>
2026-02-07 15:44:37 -0500

    Fixed import error

--- src/test/java/frc/robot/utils/calculations/TurretCalculationsTest.java
+++ src/test/java/frc/robot/utils/calculations/TurretCalculationsTest.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import frc.robot.utils.calculations.TurretCalculations;
+import frc.robot.utils.math.TurretCalculations;
 
 public class TurretCalculationsTest {
 

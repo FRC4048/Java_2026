09f4f94241ea1731dc4563780264b0eadf8fbd93
Raayed Mohammed <raayed.mo@gmail.com>
2026-02-09 20:42:33 -0500

    Reimplemented equals() ans hashCode() so that the AutoEvent class can be used in a map the way it's expected to.

--- src/main/java/frc/robot/autochooser/AutoEvent.java
+++ src/main/java/frc/robot/autochooser/AutoEvent.java
@@ -21,4 +21,31 @@ public class AutoEvent {
     return location;
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((action == null) ? 0 : action.hashCode());
+    result = prime * result + ((location == null) ? 0 : location.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    AutoEvent other = (AutoEvent) obj;
+    if (action != other.action)
+      return false;
+    if (location != other.location)
+      return false;
+    return true;
+  }
+
+  
+
 }

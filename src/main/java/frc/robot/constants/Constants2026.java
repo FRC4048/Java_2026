69853dc30a8895122dbc8a6f1baf06cbc52efe24
Joseph Kantrowitz <josephkantrowitz18@gmail.com>
2026-02-14 19:20:12 -0500

    fixed duplicate can id

--- src/main/java/frc/robot/constants/Constants2026.java
+++ src/main/java/frc/robot/constants/Constants2026.java
@@ -12,7 +12,7 @@ public class Constants2026 extends GameConstants {
     public static final int FEEDER_MOTOR_ID = 5;
     public static final int SHOOTER_MOTOR_ID = 8;
     public static final int SHOOTER_FOLLOWER_MOTOR_ID = 7;
-    public static final int TURRET_MOTOR_ID = 8;
+    public static final int TURRET_MOTOR_ID = 9;
     
     public static final double DRIVE_BASE_WIDTH = 0.635;
     public static final double DRIVE_BASE_LENGTH = 0.635;

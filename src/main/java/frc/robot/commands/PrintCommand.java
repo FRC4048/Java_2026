2d3668d07a27021dfaa16faaaf888fcfa4708b51
Sahiltheram <sahil.a.thadani@gmail.com>
2026-02-16 18:15:10 -0500

    fix priny command

--- src/main/java/frc/robot/commands/PrintCommand.java
+++ src/main/java/frc/robot/commands/PrintCommand.java
@@ -8,13 +8,25 @@ import frc.robot.utils.logging.commands.LoggableCommand;
  * <p>This class is provided by the NewCommands VendorDep
  */
 public class PrintCommand extends LoggableCommand {
+  private final String message;
+
   /**
    * Creates a new a PrintCommand.
    *
    * @param message the message to print
    */
   public PrintCommand(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public void initialize() {
     System.out.println(message);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
   }
 
   @Override

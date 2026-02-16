daffdc3c28da7c412d8df0315675388e53fd5b0a
Michael Kovalev <misha.kovalev980@gmail.com>
2026-02-16 14:49:35 -0500

    fixed PrintCommand

--- src/main/java/frc/robot/commands/PrintCommand.java
+++ src/main/java/frc/robot/commands/PrintCommand.java
@@ -1,20 +1,20 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.utils.logging.commands.LoggableCommand;
 
 /**
  * A command that prints a string when initialized.
  *
  * <p>This class is provided by the NewCommands VendorDep
  */
-public class PrintCommand extends InstantCommand {
+public class PrintCommand extends LoggableCommand {
   /**
    * Creates a new a PrintCommand.
    *
    * @param message the message to print
    */
   public PrintCommand(String message) {
-    super(() -> System.out.println(message));
+    System.out.println(message);
   }
 
   @Override

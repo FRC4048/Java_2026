db7c5f7f821ef96de6dac63785b0a58c38af4832
ohad <ohadze@yahoo.com>
2026-02-16 21:17:20 -0500

    Change the pose manager to return the vision estimate rather than the drivebase one

--- src/main/java/frc/robot/commands/auto/ExampleAuto.java
+++ src/main/java/frc/robot/commands/auto/ExampleAuto.java
@@ -8,38 +8,37 @@ import frc.robot.utils.logging.commands.LoggableCommandWrapper;
 import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
 import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
 
+//
+//public class ExampleAuto extends LoggableSequentialCommandGroup{
+//    public ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
+//        addCommands(
+//                auto.resetOdometry("ExamplePathOne"),
+//                new PrintCommand("Started ExamplePathOne"),
+//                auto.trajectoryCmd("ExamplePathOne"),
+//                new PrintCommand("Finished ExamplePathOne"),
+//                new PrintCommand("Started ExamplePathTwo"),
+//                auto.trajectoryCmd("ExamplePathTwo"),
+//                new PrintCommand("Finished ExamplePathTwo")
+//        );
+//    }
+//}
+
 
 public class ExampleAuto extends LoggableSequentialCommandGroup{
     public ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
-        addCommands(
-                auto.resetOdometry("ExamplePathOne"),
-                new PrintCommand("Started ExamplePathOne"),
-                auto.trajectoryCmd("ExamplePathOne"),
-                new PrintCommand("Finished ExamplePathOne"),
-                new PrintCommand("Started ExamplePathTwo"),
-                auto.trajectoryCmd("ExamplePathTwo"),
-                new PrintCommand("Finished ExamplePathTwo")
-        );
-    }
-}
-
-/*
-public class ExampleAuto extends LoggableSequentialCommandGroup{
-    public ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
-        addCommands(
-                auto.resetOdometry("ExamplePathOne"),
+        super(
+                LoggableCommandWrapper.wrap(auto.resetOdometry("ExamplePathOne")),
                 new LoggableParallelCommandGroup(
-                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathOne")).withTimeout(25)),
+                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathOne").withTimeout(25))),
                         new PrintCommand("Started ExamplePathOne")
                 ),
                 new PrintCommand("Finished ExamplePathOne"),
 
                 new LoggableParallelCommandGroup(
-                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathTwo")).withTimeout(25)),
+                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathTwo").withTimeout(25))),
                         new PrintCommand("Started ExamplePathTwo")
                 ),
                 new PrintCommand("Finished ExamplePathTwo")
         );
     }
 }
-*/

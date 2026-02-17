83465c2565c48dd6d9ea539f96080806cedaacd0
Sahiltheram <sahil.a.thadani@gmail.com>
2026-02-17 12:09:12 -0500

    Added Left Sequences

---
+++ src/main/java/frc/robot/commands/auto/LeftShoot.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.auto;
+
+import choreo.auto.AutoFactory;
+import frc.robot.commands.shooter.SetShootingState;
+import frc.robot.commands.shooter.SpinShooter;
+import frc.robot.constants.Constants;
+import frc.robot.constants.enums.ShootingState;
+import frc.robot.constants.enums.ShootingState.ShootState;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import frc.robot.utils.logging.commands.LoggableCommandWrapper;
+import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
+
+public class LeftShoot extends LoggableSequentialCommandGroup{
+    public LeftShoot(SwerveSubsystem subsystem, AutoFactory auto, ShooterSubsystem shooter, ShootingState shootstate) {
+        super(
+                new SetShootingState(shootstate, ShootState.SHOOTING_HUB),
+                LoggableCommandWrapper.wrap(auto.resetOdometry("LeftToTower")),
+                LoggableCommandWrapper.wrap((auto.trajectoryCmd("LeftToTower"))),
+                new SpinShooter(shooter, Constants.SHOOTER_SPEED)
+        );
+    }
+}
+

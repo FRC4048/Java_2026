be76e32f8e124aed2a09737bf47a3be6e47e623f
Raayed Mohammed <raayed.mo@gmail.com>
2026-02-17 11:33:41 -0500

    Changed the name of one constant

--- src/main/java/frc/robot/constants/ConstantsReal2026.java
+++ src/main/java/frc/robot/constants/ConstantsReal2026.java
@@ -1,16 +1,20 @@
 package frc.robot.constants;
 
-public class ConstantsReal2026 extends GameConstants {
+public class ConstantsReal2026 extends ConstantsPushbot2026 {
+
     public static final String JSON_DIRECTORY = "real";
 
+    // shooter related
     public static final int SHOOTER_MOTOR_ID = 53;
     public static final int SHOOTER_FOLLOWER_MOTOR_ID = 54;
     public static final int ANGLER_MOTOR_ID = 11;
+
+    // other CAN-ID's
     public static final int TURRET_MOTOR_ID = 1;
     public static final int FEEDER_MOTOR_ID = 2;
     public static final int HOPPER_MOTOR_ID = 3;
     public static final int INTAKE_MOTOR_ID = 4;
-    public static final int INTAKE_DEPLOY_MOTOR_ID = 5;
+    public static final int INTAKE_DEPLOYMENT_ID = 5;
     public static final int CLIMBER_MOTOR_ID = 6;
 
 }

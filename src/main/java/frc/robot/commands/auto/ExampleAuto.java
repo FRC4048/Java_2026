691b6c31e956190e6a1ffc54498281771e7716dd
Michael Kovalev <misha.kovalev980@gmail.com>
2026-02-16 21:04:26 -0500

    push

--- src/main/java/frc/robot/commands/auto/ExampleAuto.java
+++ src/main/java/frc/robot/commands/auto/ExampleAuto.java
@@ -8,22 +8,38 @@ import frc.robot.utils.logging.commands.LoggableCommandWrapper;
 import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
 import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
 
+
 public class ExampleAuto extends LoggableSequentialCommandGroup{
-    public     ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
+    public ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
         addCommands(
                 auto.resetOdometry("ExamplePathOne"),
+                new PrintCommand("Started ExamplePathOne"),
+                auto.trajectoryCmd("ExamplePathOne"),
+                new PrintCommand("Finished ExamplePathOne"),
+                new PrintCommand("Started ExamplePathTwo"),
+                auto.trajectoryCmd("ExamplePathTwo"),
+                new PrintCommand("Finished ExamplePathTwo")
+        );
+    }
+}
 
+/*
+public class ExampleAuto extends LoggableSequentialCommandGroup{
+    public ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
+        addCommands(
+                auto.resetOdometry("ExamplePathOne"),
                 new LoggableParallelCommandGroup(
-                        LoggableCommandWrapper.wrap(auto.trajectoryCmd("ExamplePathOne")),
+                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathOne")).withTimeout(25)),
                         new PrintCommand("Started ExamplePathOne")
                 ),
                 new PrintCommand("Finished ExamplePathOne"),
 
                 new LoggableParallelCommandGroup(
-                        LoggableCommandWrapper.wrap(auto.trajectoryCmd("ExamplePathTwo")),
+                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathTwo")).withTimeout(25)),
                         new PrintCommand("Started ExamplePathTwo")
                 ),
                 new PrintCommand("Finished ExamplePathTwo")
         );
     }
 }
+*/

0383cd13bc001b9c4fd0158bfa7720c01a16bd31
Raayed Mohammed <raayed.mo@gmail.com>
2026-02-17 11:17:23 -0500

    adding canid constants

--- src/main/java/frc/robot/constants/ConstantsReal2026.java
+++ src/main/java/frc/robot/constants/ConstantsReal2026.java
@@ -2,4 +2,15 @@ package frc.robot.constants;
 
 public class ConstantsReal2026 extends GameConstants {
     public static final String JSON_DIRECTORY = "real";
+
+    public static final int SHOOTER_MOTOR_ID = 53;
+    public static final int SHOOTER_FOLLOWER_MOTOR_ID = 54;
+    public static final int ANGLER_MOTOR_ID = 11;
+    public static final int TURRET_MOTOR_ID = 1;
+    public static final int FEEDER_MOTOR_ID = 2;
+    public static final int HOPPER_MOTOR_ID = 3;
+    public static final int INTAKE_MOTOR_ID = 4;
+    public static final int INTAKE_DEPLOY_MOTOR_ID = 5;
+    public static final int CLIMBER_MOTOR_ID = 6;
+
 }

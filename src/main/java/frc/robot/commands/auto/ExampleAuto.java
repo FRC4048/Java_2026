05045f276a9a9277ea3441d375a8e9525bf6418f
Michael Kovalev <misha.kovalev980@gmail.com>
2026-02-16 21:32:44 -0500

    omg it works im free

--- src/main/java/frc/robot/commands/auto/ExampleAuto.java
+++ src/main/java/frc/robot/commands/auto/ExampleAuto.java
@@ -1,6 +1,5 @@
 package frc.robot.commands.auto;
 
-
 import choreo.auto.AutoFactory;
 import frc.robot.commands.PrintCommand;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -8,34 +7,18 @@ import frc.robot.utils.logging.commands.LoggableCommandWrapper;
 import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
 import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
 
-//
-//public class ExampleAuto extends LoggableSequentialCommandGroup{
-//    public ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
-//        addCommands(
-//                auto.resetOdometry("ExamplePathOne"),
-//                new PrintCommand("Started ExamplePathOne"),
-//                auto.trajectoryCmd("ExamplePathOne"),
-//                new PrintCommand("Finished ExamplePathOne"),
-//                new PrintCommand("Started ExamplePathTwo"),
-//                auto.trajectoryCmd("ExamplePathTwo"),
-//                new PrintCommand("Finished ExamplePathTwo")
-//        );
-//    }
-//}
-
-
 public class ExampleAuto extends LoggableSequentialCommandGroup{
     public ExampleAuto(SwerveSubsystem subsystem, AutoFactory auto) {
         super(
                 LoggableCommandWrapper.wrap(auto.resetOdometry("ExamplePathOne")),
                 new LoggableParallelCommandGroup(
-                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathOne").withTimeout(25))),
+                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathOne"))),
                         new PrintCommand("Started ExamplePathOne")
                 ),
                 new PrintCommand("Finished ExamplePathOne"),
 
                 new LoggableParallelCommandGroup(
-                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathTwo").withTimeout(25))),
+                        LoggableCommandWrapper.wrap((auto.trajectoryCmd("ExamplePathTwo"))),
                         new PrintCommand("Started ExamplePathTwo")
                 ),
                 new PrintCommand("Finished ExamplePathTwo")

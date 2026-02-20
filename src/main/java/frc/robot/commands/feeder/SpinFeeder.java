c7ce2c21ac9289f6fb9cf4200fde8c6a36363e5f
Raayed Mohammed <raayed.mo@gmail.com>
2026-02-20 11:06:06 -0500

    Added back a change that was reverted when merge conflicts were being solved.

--- src/main/java/frc/robot/commands/feeder/SpinFeeder.java
+++ src/main/java/frc/robot/commands/feeder/SpinFeeder.java
@@ -4,17 +4,20 @@ import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.utils.logging.LoggedTunableNumber;
 import frc.robot.utils.logging.commands.LoggableCommand;
 
 public class SpinFeeder  extends LoggableCommand {
     
     private final FeederSubsystem subsystem;
     private final Timer timer;
-  
+    private LoggedTunableNumber tunableSpeed;
+      
     public SpinFeeder(FeederSubsystem subsystem) {
         this.subsystem = subsystem;
         timer = new Timer();
         addRequirements(subsystem);
+        tunableSpeed = new LoggedTunableNumber(FeederSubsystem.LOGGING_NAME, Constants.FEEDER_SPEED);
     }
 
     @Override
@@ -24,7 +27,7 @@ public class SpinFeeder  extends LoggableCommand {
 
     @Override
     public void execute() {
-       subsystem.setSpeed(Constants.FEEDER_SPEED);
+       subsystem.setSpeed(tunableSpeed.getAsDouble());
     }
 
     @Override

db44a721214771d2e98d36a7619119d4357f360c
Raayed Mohammed <raayed.mo@gmail.com>
2026-02-22 13:39:44 -0500

    Added more fake IDs for example subsystems to prevent crashing.

--- src/main/java/frc/robot/constants/ConstantsTestbed2026.java
+++ src/main/java/frc/robot/constants/ConstantsTestbed2026.java
@@ -10,8 +10,8 @@ package frc.robot.constants;
   */
 public class ConstantsTestbed2026 extends ConstantsPushbot2026 {
     
-    public static final int ROLLER_MOTOR_ID = 99;
-    public static final int TILT_MOTOR_ID = 98;
+    public static final int ROLLER_MOTOR_ID = 99; // fake id for example subsytem to prevent crashes
+    public static final int TILT_MOTOR_ID = 98; // fake id for example subsytem to prevent crashes
     //Intake motor id (43) isn't real
     // public static final int HOPPER_MOTOR_ID = 43;
 

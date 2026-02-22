baa20cfe110ed1a16820690005a37f585bad574e
Samhith Dewal <dewalsamhith@gmail.com>
2026-02-22 14:27:34 -0500

    Removed the DigitalInput from the intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,18 +7,12 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.commands.intake.SpinIntake;
 import frc.robot.constants.Constants;
-import frc.robot.utils.logging.input.DigitalInputLoggableInputs;
 import frc.robot.utils.logging.input.MotorLoggableInputs;
-import frc.robot.utils.logging.io.motor.DigitalInputIo;
-import frc.robot.utils.logging.io.motor.MockDigitalInputIo;
 import frc.robot.utils.logging.io.motor.MockSparkMaxIo;
-import frc.robot.utils.logging.io.motor.RealDigitalInputIo;
 import frc.robot.utils.logging.io.motor.RealSparkMaxIo;
-import frc.robot.utils.logging.io.motor.SimDigitalInputIo;
 import frc.robot.utils.logging.io.motor.SimSparkMaxIo;
 import frc.robot.utils.logging.io.motor.SparkMaxIo;
 import frc.robot.utils.simulation.MotorSimulator;
@@ -28,11 +22,9 @@ public class IntakeSubsystem extends SubsystemBase {
     
     public static final String LOGGING_NAME = "IntakeSubsystem";
     private final SparkMaxIo io;
-    private final DigitalInputIo intakeDeploymentSwitch;
 
-    public IntakeSubsystem(SparkMaxIo io, DigitalInputIo intakeDeploymentSwitch) {
+    public IntakeSubsystem(SparkMaxIo io) {
         this.io = io;
-        this.intakeDeploymentSwitch = intakeDeploymentSwitch;
         setDefaultCommand(new SpinIntake(this));
     }
 
@@ -47,11 +39,6 @@ public class IntakeSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         io.periodic();
-        intakeDeploymentSwitch.periodic();
-    }
-
-    public boolean isDeployed() {
-        return intakeDeploymentSwitch.isPressed();
     }
 
     public static SparkMaxIo createMockIo() {
@@ -66,30 +53,6 @@ public class IntakeSubsystem extends SubsystemBase {
                 new MotorSimulator(motor, visualizer.getIntakeLigament()));
     }
 
-    public static DigitalInputIo createMockDeploymentSwitch() {
-    return new MockDigitalInputIo(
-            LOGGING_NAME + "/DeploymentSwitch",
-            new DigitalInputLoggableInputs()
-    );
-}
-
-public static DigitalInputIo createRealDeploymentSwitch() {
-    return new RealDigitalInputIo(
-            LOGGING_NAME + "/DeploymentSwitch",
-            new DigitalInput(Constants.INTAKE_DIGITAL_INPUT_CHANNEL),
-            new DigitalInputLoggableInputs()
-    );
-    
-}
-
-public static DigitalInputIo createSimDeploymentSwitch() {
-    return new SimDigitalInputIo(
-        LOGGING_NAME + "/DeploymentSwitch",
-        new DigitalInput(Constants.INTAKE_DIGITAL_INPUT_CHANNEL),
-        new DigitalInputLoggableInputs()
-    );
-}
-
     private static SparkMax createMotor() {
         SparkMax motor = new SparkMax(Constants.INTAKE_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
         SparkMaxConfig motorConfig = new SparkMaxConfig();
@@ -102,7 +65,4 @@ public static DigitalInputIo createSimDeploymentSwitch() {
         return motor;
     }
     
-    public DigitalInputIo getDeploymentSwitchIo() {
-        return intakeDeploymentSwitch;
-    }
 }

efcdd7033bc7b2d380adee20cbe55c7c65ea97b6
Sahiltheram <sahil.a.thadani@gmail.com>
2026-03-10 20:34:52 -0400

    Added sequence

--- src/main/java/frc/robot/commands/climber/ClimbSequence.java
+++ src/main/java/frc/robot/commands/climber/ClimbSequence.java
@@ -1,15 +1,17 @@
 package frc.robot.commands.climber;
 
+import frc.robot.commands.drive.DriveSwerve;
+import frc.robot.constants.enums.DriveDirection;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
-import frc.robot.utils.logging.commands.LoggableCommand;
-import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
 import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
 
 public class ClimbSequence extends LoggableSequentialCommandGroup {
     public ClimbSequence(ClimberSubsystem climberSubsystem, SwerveSubsystem subsystem) {
         super(
-            new ClimberUp(climberSubsystem)
+            new ClimberUp(climberSubsystem),
+            new DriveSwerve(subsystem, DriveDirection.BACKWARD, 2, 2),
+            new ClimberDown(climberSubsystem)
             
         );
 

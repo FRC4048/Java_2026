7f6bf3af7ea5b3c5cdfb42e84ce98ecde3401d6d
Jackson Wess <wessjackson93@gmail.com>
2026-03-18 20:39:39 -0400

    do nothing parrallele command

--- src/main/java/frc/robot/commands/auto/DoNothing.java
+++ src/main/java/frc/robot/commands/auto/DoNothing.java
@@ -5,9 +5,8 @@ import frc.robot.commands.turret.RunTurretToRevLimit;
 import frc.robot.subsystems.AnglerSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
-import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
 
-public class DoNothing extends LoggableSequentialCommandGroup{
+public class DoNothing extends LoggableParallelCommandGroup{
     public DoNothing(TurretSubsystem turret, AnglerSubsystem angler) {
             super(
                 new LoggableParallelCommandGroup(

f772e6bbac445931ee9a0f3280320a5832768225
Jackson Wess <wessjackson93@gmail.com>
2026-03-21 11:06:47 -0400

    Removed comment

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -28,7 +28,6 @@ public class TurretSubsystem extends SubsystemBase {
 
     public TurretSubsystem(SparkMaxPidMotorIo io) {
         this.io = io;
-        // Create 2 tunable configs in case we want to tune both
         this.pidManager = new TunablePIDManager(LOGGING_NAME, io, createPidConfig0());
         stopMotors();
     }

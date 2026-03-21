f85eb1d6f16a36f2f5f5f076323230c17f051a8f
RoRoBout212 <rorobout212@gmail.com>
2026-03-21 11:29:59 -0400

    increased timeout counter in turret commands

--- src/main/java/frc/robot/commands/turret/RunTurretToRevLimit.java
+++ src/main/java/frc/robot/commands/turret/RunTurretToRevLimit.java
@@ -4,16 +4,19 @@ import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.utils.logging.commands.LoggableCommand;
+import frc.robot.utils.logging.TimeoutLogger;
 
 /**
  * Runs the turret to the reverse (left) limit switch and resets the encoder to zero.
  */
 public class RunTurretToRevLimit extends LoggableCommand {
+    private final TimeoutLogger timeoutCounter;
     private final TurretSubsystem turret;
     private boolean finished = false;
     private final Timer timer = new Timer();
 
     public RunTurretToRevLimit(TurretSubsystem turret) {
+        timeoutCounter = new TimeoutLogger(getName());
         this.turret = turret;
         addRequirements(turret);
     }
@@ -30,6 +33,9 @@ public class RunTurretToRevLimit extends LoggableCommand {
 
     @Override
     public void end(boolean interrupted) {
+        if (timer.hasElapsed(Constants.TURRET_TIMEOUT)) {
+            timeoutCounter.increaseTimeoutCount();
+        }
         turret.resetEncoderToZero();
         turret.stopMotors();
     }
--- src/main/java/frc/robot/commands/turret/TurretToRev.java
+++ src/main/java/frc/robot/commands/turret/TurretToRev.java
@@ -3,14 +3,17 @@ package frc.robot.commands.turret;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.TurretSubsystem;
+import frc.robot.utils.logging.TimeoutLogger;
 import frc.robot.utils.logging.commands.LoggableCommand;
 
 public class TurretToRev extends LoggableCommand{
     
+    private final TimeoutLogger timeoutCounter;
     private final TurretSubsystem turret;
     private final Timer timer = new Timer();
 
     public TurretToRev(TurretSubsystem turret) {
+        timeoutCounter = new TimeoutLogger(getName());
         this.turret = turret;
         addRequirements(turret);
     }
@@ -27,6 +30,9 @@ public class TurretToRev extends LoggableCommand{
 
     @Override
     public void end(boolean interrupted) {
+        if (timer.hasElapsed(Constants.TURRET_TIMEOUT)) {
+            timeoutCounter.increaseTimeoutCount();
+        }
         turret.stopMotors();
     }
 

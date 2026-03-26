31e27a151fb423079e386f74d90f0c41e5ea2d02
Jackson Wess <wessjackson93@gmail.com>
2026-03-26 19:14:35 -0400

    commit

--- src/main/java/frc/robot/commands/lightStrip/SetLed.java
+++ src/main/java/frc/robot/commands/lightStrip/SetLed.java
@@ -12,10 +12,12 @@ public class SetLed extends LoggableCommand{
 
     private final LightStripSubsystem lightStrip;
     private ShootingState shootingState;
+    private SwerveSubsystem drivebase;
   
-    public SetLed(LightStripSubsystem lightStrip, ShootingState shootingState) {
+    public SetLed(LightStripSubsystem lightStrip, ShootingState shootingState, SwerveSubsystem drivebase) {
 
         this.lightStrip = lightStrip;
+        this.drivebase = drivebase;
         this.shootingState = shootingState;
         addRequirements(lightStrip);
 
@@ -28,11 +30,38 @@ public class SetLed extends LoggableCommand{
 
     @Override
     public void execute() {
-        switch(shootingState.getShootState()){
-            case STOPPED -> lightStrip.setPattern(BlinkinPattern.BLACK);
-            case SHUTTLING -> lightStrip.setPattern(BlinkinPattern.RAINBOW_RAINBOW_PALETTE);
-            case SHOOTING_HUB -> lightStrip.setPattern(BlinkinPattern.GREEN);
-            case FIXED -> lightStrip.setPattern(BlinkinPattern.AQUA);
+        double x = drivebase.getPose().getX();
+        double y = drivebase.getPose().getY();
+
+        // If statement checks if the robot is near the trench
+        if (x > Trench.RED_BOTTOM_LOWER.getX() && x < Trench.RED_BOTTOM_HIGHER.getX() && y > Trench.RED_BOTTOM_LOWER.getY() && y < Trench.RED_BOTTOM_HIGHER.getY() ||
+            x > Trench.RED_TOP_LOWER.getX() && x < Trench.RED_TOP_HIGHER.getX() && y > Trench.RED_TOP_LOWER.getY() && y < Trench.RED_TOP_HIGHER.getY() ||
+            x > Trench.BLUE_BOTTOM_LOWER.getX() && x < Trench.BLUE_BOTTOM_HIGHER.getX() && y > Trench.BLUE_BOTTOM_LOWER.getY() && y < Trench.BLUE_BOTTOM_HIGHER.getY() ||
+            x > Trench.BLUE_TOP_LOWER.getX() && x < Trench.BLUE_TOP_HIGHER.getX() && y > Trench.BLUE_TOP_LOWER.getY() && y < Trench.BLUE_TOP_HIGHER.getY()) {
+
+                if (shootingState.getShootState() != ShootState.STOPPED) { // Light strip only blinks if the shooting state is not stopped
+                    lightStrip.setPattern(BlinkinPattern.STROBE_RED);
+                }
+
+        } else {
+            switch (shootingState.getShootState()) {
+            case STOPPED:
+                lightStrip.setPattern(BlinkinPattern.BLACK);
+                break;
+            case FIXED:
+                lightStrip.setPattern(BlinkinPattern.AQUA);
+                break;
+            case FIXED_2:
+                lightStrip.setPattern(BlinkinPattern.AQUA);
+                break;
+            case SHOOTING_HUB:
+                lightStrip.setPattern(BlinkinPattern.GREEN);
+                break;
+            case SHUTTLING:
+                lightStrip.setPattern(BlinkinPattern.RAINBOW_RAINBOW_PALETTE);
+                break;
+            }
+
         }
     }
 

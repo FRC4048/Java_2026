3fb6ce1e72455357a706ab4d8267a9f59f008021
Jackson Wess <wessjackson93@gmail.com>
2026-03-30 18:56:30 -0400

    removed clear auto command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.autochooser.FieldLocation;
-import frc.robot.commands.auto.ClearAutonomousCommand;
 import frc.robot.commands.shooter.SetShootingState;
 import frc.robot.constants.Constants;
 import frc.robot.utils.BlinkinPattern;
@@ -93,11 +92,6 @@ public class Robot extends LoggedRobot {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         robotContainer = new RobotContainer();
-        if(Constants.DEBUG){
-        SmartDashboard.putData(
-                "Auto/Clear Autonomous Command",
-                new ClearAutonomousCommand(this::clearAutonomousCommand, robotContainer.getShootingState()));
-        }
     }
 
     public static RobotMode getMode() {

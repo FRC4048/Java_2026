3c877557628e5d14bd7e36199c9c3d2d18bf22e0
Michael Kovalev <misha.kovalev980@gmail.com>
2026-03-31 19:09:00 -0400

    added fast depot sequence, updated fast depot path, autochooser and auto action

---
+++ src/main/java/frc/robot/commands/auto/newauto/FastDepot.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.auto.newauto;
+
+import choreo.auto.AutoFactory;
+import frc.robot.commands.auto.AutoReset;
+import frc.robot.commands.auto.AutoShoot;
+import frc.robot.commands.shooter.SetShootingState;
+import frc.robot.constants.enums.ShootingState;
+import frc.robot.constants.enums.ShootingState.ShootState;
+import frc.robot.subsystems.*;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import frc.robot.utils.logging.commands.LoggableCommandWrapper;
+import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
+import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
+
+public class FastDepot extends LoggableSequentialCommandGroup {
+    public FastDepot(
+            SwerveSubsystem drivetrain, AutoFactory auto, ShooterSubsystem shooter, ShootingState shootstate,
+            HopperSubsystem hopper, FeederSubsystem feeder, TurretSubsystem turret, AnglerSubsystem angler,
+            ControllerSubsystem controller, IntakeDeployerSubsystem intake) {
+        super(  
+            new LoggableParallelCommandGroup(
+                LoggableCommandWrapper.wrap(auto.resetOdometry("Depot_Fast")),
+                LoggableCommandWrapper.wrap(auto.trajectoryCmd("Depot_Fast").withTimeout(10)),
+                new AutoReset(shootstate, turret, angler),
+                new SetShootingState(shootstate, ShootState.AUTO_AIM)
+            ),
+            new AutoShoot(hopper, feeder, 0)
+        );
+    }
+}

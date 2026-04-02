9c67dd258000839fbfaa4e83f7ce6ed806d6f2d1
Jackson Wess <wessjackson93@gmail.com>
2026-04-02 19:52:51 -0400

    Fixed fast depot sequence

--- src/main/java/frc/robot/commands/auto/newauto/FastDepot.java
+++ src/main/java/frc/robot/commands/auto/newauto/FastDepot.java
@@ -1,9 +1,14 @@
 package frc.robot.commands.auto.newauto;
 
 import choreo.auto.AutoFactory;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.auto.AutoReset;
 import frc.robot.commands.auto.AutoShoot;
+import frc.robot.commands.drive.DriveSwerve;
+import frc.robot.commands.intakeDeployment.ToggleDeployment;
 import frc.robot.commands.shooter.SetShootingState;
+import frc.robot.constants.enums.DriveDirection;
 import frc.robot.constants.enums.ShootingState;
 import frc.robot.constants.enums.ShootingState.ShootState;
 import frc.robot.subsystems.*;
@@ -25,7 +30,19 @@ public class FastDepot extends LoggableSequentialCommandGroup {
                 LoggableCommandWrapper.wrap(auto.resetOdometry("Depot_Fast")),
                 LoggableCommandWrapper.wrap(auto.trajectoryCmd("Depot_Fast"))
             ),
-            new AutoShoot(hopper, feeder, 5)
+            new AutoShoot(hopper, feeder, 2),
+            new ToggleDeployment(intake, controller),
+            new LoggableWaitCommand(0.5),
+            new LoggableParallelCommandGroup(
+                new AutoShoot(hopper, feeder, 20),
+                new LoggableSequentialCommandGroup(
+                    new DriveSwerve(drivetrain, DriveDirection.FORWARD, 5, 0.2),
+                    new DriveSwerve(drivetrain, DriveDirection.BACKWARD, 0.5, 0.5),
+                    new DriveSwerve(drivetrain, DriveDirection.FORWARD, 6, 0.2),
+                    new ToggleDeployment(intake, controller)
+                )
+            )
+
         );
     }
 }

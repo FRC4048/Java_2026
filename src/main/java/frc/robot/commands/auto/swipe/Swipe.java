6bba5c71138aaf3d71931b57ca81974e35a5cd1f
Jackson Wess <wessjackson93@gmail.com>
2026-04-13 19:41:33 -0400

    Updated auto

--- src/main/java/frc/robot/commands/auto/swipe/Swipe.java
+++ src/main/java/frc/robot/commands/auto/swipe/Swipe.java
@@ -1,14 +1,6 @@
 package frc.robot.commands.auto.swipe;
 
-import java.util.function.Supplier;
-
 import choreo.auto.AutoFactory;
-import choreo.auto.AutoTrajectory;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Robot;
 import frc.robot.commands.auto.AutoReset;
 import frc.robot.commands.auto.AutoShoot;
 import frc.robot.commands.drive.DriveSwerve;
@@ -22,7 +14,6 @@ import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import frc.robot.utils.logging.commands.LoggableCommandWrapper;
 import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
 import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
-import frc.robot.utils.logging.commands.LoggableWaitCommand;
 
 public class Swipe extends LoggableSequentialCommandGroup {
 

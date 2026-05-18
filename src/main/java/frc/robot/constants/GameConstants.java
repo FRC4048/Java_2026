567ddc213f931b95ceafd38b366e41c377f92cad
ohad <ohadze@yahoo.com>
2026-05-18 18:35:21 -0400

    PR comments

--- src/main/java/frc/robot/constants/GameConstants.java
+++ src/main/java/frc/robot/constants/GameConstants.java
@@ -117,7 +117,7 @@ public class GameConstants {
     public static final double TURRET_SHORT_RANGE_I = 0.000000;
     public static final double TURRET_SHORT_RANGE_D = 0.001;
     public static final double TURRET_SHORT_RANGE_FF = 0.0;
-    public static final double TURRET_LONG_RANGE_P = 0.002;
+    public static final double TURRET_LONG_RANGE_P = 1;
     public static final double TURRET_LONG_RANGE_I = 0.000000;
     public static final double TURRET_LONG_RANGE_D = 0.0;
     public static final double TURRET_LONG_RANGE_FF = 0.0;
